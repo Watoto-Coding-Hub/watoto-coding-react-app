e68551b643c5a3cf5ac783b0566f493100d4165a
IanMcbull <ianmugenya@gmail.com>
2022-11-29 12:58:04 +0300

    header changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     < div className="bg-green-1">
        <Header/>
-       <HeroSection/>
+       {/* <HeroSection/> */}
        <About/>
        <Team/>
        <Fundraising/>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,36 @@
+import logo from "../assets/media/new-logo-watoto.jpg"
 function Header() {
   return (
-    <nav className="bg-green-1 p-4 flex space-x-8 text-lg text-white w-full justify-between">
-       <div className="flex space-x-8">
-       <a href="#">Kibera</a>
-       <a href="#">Team</a>
-       <a href="#">Fundraising</a>
-       <a href="#">Contact</a>
+   <header className="min-h-[80vh] bg-grey-1 flex flex-col">
+     <nav className="bg-green-1 px-8 py-6 flex space-x-8 text-lg text-black w-full justify-between container mx-auto bg-white rounded-full">
+       <div className="flex flex-col">
+       <div className="flex justify-between">
+       
+       </div>
+       <ul className="flex space-x-8">
+       <li><a href="#">Kibera</a></li>
+       <li><a href="#">Team</a></li>
+       <li><a href="#">Fundraising</a></li>
+       <li><a href="#">Contact</a></li>
+       </ul>
        </div>
     </nav>
+     
+     {/* <div className="header-container">
+      <img />
+     <nav className="bg-green-1 px-8 py-6 flex space-x-8 text-lg text-black w-full justify-between container mx-auto bg-white rounded-full">
+       <ul className="flex space-x-8">
+       <li><a href="#">Kibera</a></li>
+       <li><a href="">Team</a></li>
+       <li><a href="">Fundraising</a></li>
+       <li><a href="">Contact</a></li>
+       </ul>
+    </nav>
+    <div className="header-content">
+       <h1></h1>
+    </div>
+     </div> */}
+   </header>
   )
 }
 
