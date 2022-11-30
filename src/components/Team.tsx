ebdad3c7a079ba8d0ed2db2fbd5bd709f93f8840
IanMcbull <ianmugenya@gmail.com>
2022-11-30 21:26:59 +0300

    Team section edits

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,15 +3,15 @@ import CostanzaImage from '../assets/media/costanzaprofile.jpg'
 
 function Team() {
   return (
-    <div className="Team bg-blue-2 ">
+    <article className="Team bg-blue-2 min-h-[90vh] flex flex-col justify-center items-center">
       <h1 className='text-center font-bold text-3xl pt-4'> OUR TEAM</h1>
-      <div className=" grid grid-cols-2 max-sm:flex flex-col">
-        <div className='max-w-lg bg-red rounded-lg ml-16 my-6' >
- <img src={LeonardImage} alt="An image of Leonard" className="w-48 h-48 rounded-full m-auto mt-4 "/> 
+      <div className=" grid grid-cols-2 max-sm:flex flex-col mt-8 gap-6">
+        <div className='max-w-3xl bg-red rounded-lg  p-4' >
+ <img src={LeonardImage} alt="An image of Leonard" className="w-48 h-48 rounded-full m-auto mt-4 object-cover"/> 
 
  <div>
  <h2 className='text-bold text-center mt-4 font-bold text-xl'>Leonard Onyango</h2>
- <p className='text-xl px-6 pt-4 pb-4 max-sm:py-10 text-dark'>
+ <p className='text-xl px-6 pt-4 pb-4 max-sm:py-10 text-dark text-justify'>
    He was born & raised in Kibera Slums, he is amongst the 
 lucky students who got sponsorship to pursue further education in the midst of harsh conditions.
 He taught basic computer skills and graphic design in different colleges. 
@@ -21,17 +21,17 @@ teaching kids ICT in creative ways.</p>
 </div>
 </div>
 
- <div className=' bg-green max-w-lg rounded-lg m-6'>
-<img src={CostanzaImage} alt="An image of Costanza" className="h-48 w-48 rounded-full m-auto mt-4 "/>
+ <div className=' bg-green max-w-3xl rounded-lg'>
+<img src={CostanzaImage} alt="An image of Costanza" className="h-48 w-48 rounded-full m-auto mt-4 object-cover"/>
 <div>
   <h2 className='text-center mt-4 font-bold text-xl'>Costanza Casullo</h2>
-<p className='text-xl px-6 pt-10 max-sm:py-6 text-dark'> She is from Italy and joined Watoto Coding as a volunteer in 2022. She is a techie passionate about web design and game development and currently 
+<p className='text-xl px-6 pt-10 max-sm:py-6 text-dark text-justify'> She is from Italy and joined Watoto Coding as a volunteer in 2022. She is a techie passionate about web design and game development and currently 
   helps Watoto Coding with social media, outreach, creating the website and curriculum development.</p>
   </div>
 </div> 
 
     </div>
-    </div>
+    </article>
   )
 }
 
