977b75c7ae757272a36db261679b87e02316ca6b
George-Okumu <gokumu12@gmail.com>
2023-10-23 21:50:05 +0300

    text change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,11 @@ function Hero() {
       <h2 className="text-yellow-1 font-Poppins md:mt-0 mt-12">A Hub for learners within marginalized communities</h2>
       <h1 className="text-5xl font-extrabold">WATOTO CODING HUB</h1>
       </div>
-      <p className="text-xl max-w-xl">A charity teaching tech-skills to the kids of the Kibera slums. There is so much untapped potential in marginalized communities and to cultivate it we make technology accessible.</p>
+      <p className="text-xl max-w-xl">We expose children and young people from underserved communities 
+        to modern tech skills to dramatically change the course of their lives.</p>
+
+      <p className="text-sm max-w-xl">As a Kenyan registered nonprofit, we untap the talents that 
+        are still undiscovered due to the lack of access to technology. </p>
       <button className='bg-red p-5 rounded-tl-3xl rounded-br-3xl w-44 transition ease-linear hover:-translate-y-1 hover:scale-110'>Learn More</button>
     </section>
     <Plyr source={{
